081e3245e3ee4cd1a590f341885271a02d2181ee
SHREYAS <98221778+SHREYK213@users.noreply.github.com>
2022-04-07 22:50:37 +0530

    Add files via upload

---
+++ Manage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+namespace FileManagement
+{
+    class Manage
+    {
+        static void Main(string[] args)
+        {
+            string filepath = @"C:\Users\avita\OneDrive\Desktop\PROJECT C# .NET\MyFile.txt";
+            // File.WriteAllText(filepath,$"Line 1{Environment.NewLine}Line 2");
+            Console.WriteLine(File.ReadAllText(filepath));
+            string[]lines = File.ReadAllLines(filepath);
+            Console.WriteLine(lines[0]);
+        }
+    }
+}
