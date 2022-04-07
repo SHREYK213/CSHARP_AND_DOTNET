2c0ae84066f535d02bec08c8591c4e21a1470e1f
SHREYAS <98221778+SHREYK213@users.noreply.github.com>
2022-04-07 22:50:02 +0530

    Add files via upload

---
+++ Handling.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace ExceptionHandling
+{
+    class Handling
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Console.WriteLine(Divide(10,0));
+            }
+            
+            //catch (ArgumentException)
+            //{
+            //    Console.WriteLine("Invalid argument..");
+            //}
+
+            catch  //(Exception)
+            {
+                Console.WriteLine("Oops,something went wrong");
+            }
+        }
+
+        static int Divide(int a, int b)
+        {
+            //throw new Exception();
+            if (b==0)
+            {
+                throw new ArgumentException("Argument must not be zero", nameof(b));
+            }
+            //try
+            //{
+                return a / b;
+            //}
+            //catch
+            //{
+            //    return -1;
+            //}
+        }
+    }
+}
