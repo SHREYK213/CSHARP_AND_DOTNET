cda9ed1f9b0acc16114ccd26e2c3ee8f0d3a082f
SHREYAS <98221778+SHREYK213@users.noreply.github.com>
2022-04-07 22:52:34 +0530

    Add files via upload

---
+++ Loops.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Loops
+{
+    class Loops
+    {
+        static void Main(string[] args)
+        {
+            foreach (var item in "TEST")
+            {
+                Console.WriteLine(item);
+            }      
+        }
+    }
+}
