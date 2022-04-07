89d3181399a63b22a11ecc451af29e3ac5f337dc
SHREYAS <98221778+SHREYK213@users.noreply.github.com>
2022-04-07 22:48:31 +0530

    Add files via upload

---
+++ Compile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace CompilationModel
+{
+    class Compile
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey, How are you?");
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
