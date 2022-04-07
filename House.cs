4e19da197aed5cdf2b1aa5b0367d81719dcdb3aa
SHREYAS <98221778+SHREYK213@users.noreply.github.com>
2022-04-07 22:48:54 +0530

    Add files via upload

---
+++ House.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Types
+{
+    struct House
+    {
+        public House(int numberOfWindows, string color)
+        {
+            NumberOfWindows = numberOfWindows;
+            Color = color;
+        }
+
+        public int NumberOfWindows { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ Type.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace Types
+{
+    class Type
+    {
+        static void Main(string[] args)
+        {
+            //House house = new House(10, "Blue");
+            //DoStuffTOHouse(house);
+            //Console.WriteLine(house.NumberOfWindows);
+            //Console.WriteLine(house.Color);
+
+            //House house =new House(1,"Yellow");
+            //house = null;
+            //Console.WriteLine(house);
+
+            //int? i =null;
+
+            //StringBuilder s = new StringBuilder("test");
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    s.Append("!");
+            //}
+            //Console.WriteLine(s);
+           
+        }
+
+        static void DoStuffTOHouse(House house)
+        {
+            house.NumberOfWindows--;
+            house.Color="Red";
+        }
+    }
+}
