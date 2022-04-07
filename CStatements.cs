66127115909b08bf6b8b12821f4a424368fb7882
SHREYAS <98221778+SHREYK213@users.noreply.github.com>
2022-04-07 22:52:56 +0530

    Add files via upload

---
+++ CStatements.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace ConditionalStatements
+{
+    class CStatements
+    {
+        static void Main(string[] args)
+        {
+            bool isYes = true;
+            int age = 20;
+            if (isYes && age > 10)
+                Console.WriteLine("Yes");
+            else if (!isYes)
+                Console.WriteLine("hmmm");
+            else
+            {
+                Console.WriteLine("no");
+                Console.WriteLine("maybe");
+            }
+            switch (age)
+            {
+                case 10: Console.WriteLine("Ten"); break;
+                case 20: Console.WriteLine("Twenty"); break;
+                default:
+                    Console.WriteLine("Anything else"); break;
+            }
+                
+        }
+    }
+}
