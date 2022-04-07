526da059957e65ea676ef00c73541a00ba48b2b8
SHREYAS <98221778+SHREYK213@users.noreply.github.com>
2022-04-07 22:53:17 +0530

    Add files via upload

---
+++ Methods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Methods
+{
+    class Methods
+    {
+        static void Main(string[] args)
+        {
+            var personInfo = GetPersonInfo();
+            Console.WriteLine(personInfo.name);
+            Console.WriteLine(personInfo.age);
+
+        }
+        static (string name, int age) GetPersonInfo()
+        {
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("enter your age");
+            int age =int.Parse(Console.ReadLine());
+            
+            return (name, age);
+        }
+    }
+}
