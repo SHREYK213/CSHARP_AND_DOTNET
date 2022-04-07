8c89f9f0774e952434f3cb4049145600ce024644
SHREYAS <98221778+SHREYK213@users.noreply.github.com>
2022-04-07 22:51:04 +0530

    Add files via upload

---
+++ Human.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    internal class Human
+    {
+        //private int age;
+
+        //public int Age
+        //{
+        //    get { return age; }
+        //    set 
+        //    {
+        //        if(value>0)
+        //            age = value;
+        //        else 
+        //            age = 0;
+        //    }
+        //}
+
+        public Human(int age)
+        {
+           // Age = age;
+        }
+
+        public int Age { get; } = 30;
+
+    }
+}
---
+++ Propert.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Properties
+{
+    class Propert
+    {
+        static void Main(string[] args)
+        {
+            Human human = new Human(10);
+
+            // Set
+            //human.Age = -10;
+            //human.Age = 5;
+
+            // Get
+            Console.WriteLine(human.Age);
+        }
+    }
+}
