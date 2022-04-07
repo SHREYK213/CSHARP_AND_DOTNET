35020e16a52c5fcd64ff6b732ae9f149911f52d1
SHREYAS <98221778+SHREYK213@users.noreply.github.com>
2022-04-07 22:51:37 +0530

    Add files via upload

---
+++ CAO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ClassesAndObjects
+{
+    class COA
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Zoro");
+            Person person2 = new Person("luffy");
+
+            person1.age = 28;
+            person1.age = 20;
+
+            Console.WriteLine(person1.name);
+            Console.WriteLine(person1.age);
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+{
+    internal class Person
+    {
+        public Person(string name)
+        {
+            this.name = name;
+        }
+        public string name;
+        public int age;
+    }
+}
